0d34cb29812a13f75fa3d433d9383719edeeae1f
biancagit-606 <biancafiedalan31@yahoo.com>
2023-05-24 21:05:36 +1200

    added code logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const MousePosition = ({ render }) => {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  })
+
+  useEffect( () => {
+    const handleMouseMove = (e) => {
+      setMousePosition({ 
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+  
+    window.addEventListener("mousemove", handleMouseMove)
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove)
+    }
+  }
+  , [])
+
+  return render({mousePosition})
+}
+
+const PanelMouseLogger = () => {
+
+  return(
+    <div className='panel'>
+    <MousePosition render={({mousePosition}) => (
+      <>
+        <p>x coordinate: {mousePosition.x}</p>
+        <p>y coordinate: {mousePosition.y}</p>
+      </>
+    )} 
+    />
+  </div>
+  )
+}
+
+const PointMouseLogger = () => {
+
+  return (
+    <MousePosition render={({mousePosition}) => (
+      <p>({mousePosition.x}, {mousePosition.y})</p>
+    )}
+    />
+  )
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1>Mouse position tracker</h1>
+      <PanelMouseLogger />
+      <PointMouseLogger />
     </div>
+
   );
 }
 
